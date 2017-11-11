7c80e4a682cddc4c306d112664bed8242a70db6e
Mahendra Tennakoon <mahendrathennakoon@gmail.com>
2017-11-11 08:48:53 +0530

    Application startup logs

--- src/main/java/controllers/Main.java
+++ src/main/java/controllers/Main.java
@@ -5,11 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main extends Application {
+    static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        LOG.info("Starting application ------------------------------------------------------------------------------");
+
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("views/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
