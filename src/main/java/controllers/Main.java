081de7365be52c4b00fbb656c7a4ba2792107442
Mahendra Tennakoon <mahendrathennakoon@gmail.com>
2017-11-11 09:26:50 +0530

    Login

--- src/main/java/controllers/Main.java
+++ src/main/java/controllers/Main.java
@@ -5,20 +5,28 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 public class Main extends Application {
     static final Logger LOG = LoggerFactory.getLogger(Main.class);
+    private Stage stage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         LOG.info("Starting application ------------------------------------------------------------------------------");
 
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("views/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        stage = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("views/login.fxml"));
+        Scene loginScene = new Scene(root);
+
+        stage.setScene(loginScene);
+        stage.setResizable(false);
+        stage.initStyle(StageStyle.UNDECORATED);
+
+        stage.show();
     }
 
 
---
+++ src/main/java/models/UserAccountEntity.java
@@ -0,0 +1,116 @@
+package models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_account", schema = "procurement_db", catalog = "")
+public class UserAccountEntity {
+    private String userName;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String accType;
+    private String accessPrivileges;
+
+    @Id
+    @Column(name = "user_name")
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Basic
+    @Column(name = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Basic
+    @Column(name = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Basic
+    @Column(name = "first_name")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Basic
+    @Column(name = "last_name")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Basic
+    @Column(name = "acc_type")
+    public String getAccType() {
+        return accType;
+    }
+
+    public void setAccType(String accType) {
+        this.accType = accType;
+    }
+
+    @Basic
+    @Column(name = "access_privileges")
+    public String getAccessPrivileges() {
+        return accessPrivileges;
+    }
+
+    public void setAccessPrivileges(String accessPrivileges) {
+        this.accessPrivileges = accessPrivileges;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserAccountEntity that = (UserAccountEntity) o;
+
+        if (userName != null ? !userName.equals(that.userName) : that.userName != null) return false;
+        if (password != null ? !password.equals(that.password) : that.password != null) return false;
+        if (email != null ? !email.equals(that.email) : that.email != null) return false;
+        if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(that.lastName) : that.lastName != null) return false;
+        if (accType != null ? !accType.equals(that.accType) : that.accType != null) return false;
+        if (accessPrivileges != null ? !accessPrivileges.equals(that.accessPrivileges) : that.accessPrivileges != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userName != null ? userName.hashCode() : 0;
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (accType != null ? accType.hashCode() : 0);
+        result = 31 * result + (accessPrivileges != null ? accessPrivileges.hashCode() : 0);
+        return result;
+    }
+}
