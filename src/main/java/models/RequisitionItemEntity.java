3a77ba93f25d459c594e33a397c09c8216164cbf
Mahendra Tennakoon <mahendrathennakoon@gmail.com>
2017-11-25 07:57:00 +0530

    RequisitionItem model

--- src/main/java/models/RequisitionItemEntity.java
+++ src/main/java/models/RequisitionItemEntity.java
@@ -4,9 +4,13 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "requisition_item", schema = "procurement_db", catalog = "")
+@IdClass(RequisitionItemEntityPK.class)
 public class RequisitionItemEntity {
     private String reqId;
+    private String itemName;
     private Integer qty;
+    private String status;
+    private String date;
 
     @Id
     @Column(name = "reqID")
@@ -16,6 +20,16 @@ public class RequisitionItemEntity {
 
     public void setReqId(String reqId) {
         this.reqId = reqId;
+    }
+
+    @Id
+    @Column(name = "itemName")
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
     }
 
     @Basic
@@ -28,6 +42,26 @@ public class RequisitionItemEntity {
         this.qty = qty;
     }
 
+    @Basic
+    @Column(name = "status")
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Basic
+    @Column(name = "date")
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -36,7 +70,10 @@ public class RequisitionItemEntity {
         RequisitionItemEntity that = (RequisitionItemEntity) o;
 
         if (reqId != null ? !reqId.equals(that.reqId) : that.reqId != null) return false;
+        if (itemName != null ? !itemName.equals(that.itemName) : that.itemName != null) return false;
         if (qty != null ? !qty.equals(that.qty) : that.qty != null) return false;
+        if (status != null ? !status.equals(that.status) : that.status != null) return false;
+        if (date != null ? !date.equals(that.date) : that.date != null) return false;
 
         return true;
     }
@@ -44,7 +81,10 @@ public class RequisitionItemEntity {
     @Override
     public int hashCode() {
         int result = reqId != null ? reqId.hashCode() : 0;
+        result = 31 * result + (itemName != null ? itemName.hashCode() : 0);
         result = 31 * result + (qty != null ? qty.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = 31 * result + (date != null ? date.hashCode() : 0);
         return result;
     }
 }
